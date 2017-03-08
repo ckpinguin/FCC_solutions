6f9becbced0eecea23345df346ea167f9a636a87
Christof Kälin <christof.kaelin@gmail.com>
2017-03-08 18:41:21 +0100

    Update program.js

--- nodejs/06_make_it_modular/program.js
+++ nodejs/06_make_it_modular/program.js
@@ -5,7 +5,6 @@ const filter = process.argv[3];
 
 mymodule(dir, filter, function(err, res) {
     if (err) throw err;
-    console.log(typeof(res));
     for (let item of res) {
         console.log(item);
     }
