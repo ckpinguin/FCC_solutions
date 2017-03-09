da1d4db1b35f733d19944d1b6fd3950e41fd16b9
Christof Kälin <christof.kaelin@gmail.com>
2017-03-09 11:42:59 +0100

    12

---
+++ nodejs/12_http_uppercaserer/program.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const port = process.argv[2];
+
+const server = http.createServer(function (req, res) {
+    req.setEncoding('utf8');
+    let chunks = [];
+    req.on('data', chunk => {
+        chunks.push(chunk.toString());
+        res.write(chunk.toUpperCase());
+    });
+    req.on('end', () => {
+        res.end();
+    });
+}).listen(port);
